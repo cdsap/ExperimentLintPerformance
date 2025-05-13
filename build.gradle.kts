b1cd1e967b399414e76f16ef26613617866202fa
Inaki Villar <inaki.seri@gmail.com>
2025-05-12 21:38:05 -0700

    agp 8.8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version("2.1.20") apply false
     id("org.jetbrains.kotlin.plugin.compose") version("2.1.20") apply false
-    id("com.android.application") version "8.9.0" apply false
-    id("com.android.library") version "8.9.0" apply false
+    id("com.android.application") version "8.8.2" apply false
+    id("com.android.library") version "8.8.2" apply false
     id ("io.github.cdsap.kotlinprocess") version "0.1.7"
     id ("io.github.cdsap.gradleprocess") version "0.1.3"
     id ("io.github.cdsap.gcreport") version "0.1.0"
